16210c1d183bdc9e0bd4dcd641a7c9c88f465814
Mike A. <git@mikealmel.ooo>
2025-02-16 21:39:52 +0100

    Add basic unit tests

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  unstable = import (fetchTarball https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz) { };
+in
 pkgs.mkShell {
   packages = with pkgs; [
-    python312
-    uv
+    (unstable.python312.withPackages (ps: [ps.uv]))
     nodejs_23
     wrangler
   ];
